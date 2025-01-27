e624b19ba3ddaf59403321b259c6895e819a2a66
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-01-27 21:20:24 +0500

    feat: delete add item in grociery component

--- src/components/grociery.js
+++ src/components/grociery.js
@@ -3,21 +3,10 @@ import '../styles/grociery.css';
 import ItemsList from './itemsList'
 
 function Grociery({items, handleCheckbox, deleteItem}) {
-
-    const getValue = (e) => {
-        console.log(e.target.value);
-        let grocieryItem = e.target.value;
-        return grocieryItem;
-    }
     
-    const addItem = (grocieryItem) => {
-       
-    }
 
     return (
         <main>
-            <input onChange = {getValue} type = "text" placeholder = "Enter the grociery items..." />
-            <button onClick={addItem}>Add item</button>
             { items.length ? 
                 (
                    <ItemsList items={items}
